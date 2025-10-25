ab964f2b513cd9586b4a7c4da8cbb298153d3104
Brett Adams <brett@Mac.local>
2025-10-25 14:28:51 -0600

    add foreign keys

--- wfo_plantlist_to_sqlite.sh
+++ wfo_plantlist_to_sqlite.sh
@@ -3,43 +3,6 @@
 # Usage ./wfo_plantlist_to_sqlite.sh wfo_plantlist_2025-06.db ../wfo_plantlist_2025-06
 
 #
-# Create the name table
-#
-sqlite3 "$1" <<'SQL'
-pragma journal_mode = memory;
-pragma foreign_keys = on;
-
-drop table if exists name;
-
-create table name (
-    ID text,
-    alternativeID text,
-    basionymID text,
-    scientificName text,
-    authorship text,
-    rank text,
-    uninomial text,
-    genus text,
-    infragenericEpithet text,
-    specificEpithet text,
-    infraspecificEpithet text,
-    code text,
-    referenceID text,
-    publishedInYear text,
-    link text
-);
-
--- create index name_id_idx on name (ID);
--- create index name_scientific_name_idx on name (scientificName);
-SQL
-
-#
-# Import the name table
-#
-echo "Importing the name table..."
-sqlite3 "$1" ".mode tabs" ".import --skip 1 $2/name.tsv name"
-
-#
 # Create the reference table
 #
 sqlite3 "$1" <<'SQL'
@@ -67,6 +30,45 @@ echo "Importing the reference table..."
 sqlite3 "$1" ".mode tabs" ".import --skip 1 $2/reference.tsv reference"
 
 #
+# Create the name table
+#
+sqlite3 "$1" <<'SQL'
+pragma journal_mode = memory;
+pragma foreign_keys = on;
+
+drop table if exists name;
+
+create table name (
+    ID text,
+    alternativeID text,
+    basionymID text,
+    scientificName text,
+    authorship text,
+    rank text,
+    uninomial text,
+    genus text,
+    infragenericEpithet text,
+    specificEpithet text,
+    infraspecificEpithet text,
+    code text,
+    referenceID text,
+    publishedInYear text,
+    link text,
+    foreign key (referenceID) references reference(ID)
+    foreign key (basionymID) references name(ID)
+);
+
+-- create index name_id_idx on name (ID);
+-- create index name_scientific_name_idx on name (scientificName);
+SQL
+
+#
+# Import the name table
+#
+echo "Importing the name table..."
+sqlite3 "$1" ".mode tabs" ".import --skip 1 $2/name.tsv name"
+
+#
 # Create the synonym table
 #
 sqlite3 "$1" <<'SQL'
@@ -81,7 +83,10 @@ create table synonym (
     nameID text,
     accordingToID text,
     referenceID text,
-    link text
+    link text,
+    foreign key (accordingToID) references reference(ID),
+    foreign key (taxonID) references taxon(ID),
+    foreign key (nameID) references name(ID)
 );
 
 -- create index synonym_id_idx on synonym (id);
@@ -113,8 +118,9 @@ create table taxon (
     scrutinizerDate text,
     referenceID text,
     link text,
-
-    foreign key(nameID) references name(ID)
+    foreign key(nameID) references name(ID),
+    foreign key(parentID) references taxon(ID),
+    foreign key (accordingToID) references reference(ID)
 );
 
 create index taxon_id_idx on taxon (id);
@@ -142,7 +148,8 @@ create table typematerial (
     ID text,
     nameID text,
     citation text,
-    link text
+    link text,
+    foreign key(nameID) references name(ID)
 );
 
 SQL
